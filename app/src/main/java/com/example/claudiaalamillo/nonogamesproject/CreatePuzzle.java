55aae76509128f3e0a0154f41fb0e82816d90015
blueroes112 <ireneyang112@gmail.com>
2017-10-20 10:01:48 -0700

    linked back button of create puzzle activity and created level selection activity

--- app/src/main/java/com/example/claudiaalamillo/nonogamesproject/CreatePuzzle.java
+++ app/src/main/java/com/example/claudiaalamillo/nonogamesproject/CreatePuzzle.java
@@ -1,16 +1,33 @@
 package com.example.claudiaalamillo.nonogamesproject;
 
+import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 
 
 public class CreatePuzzle extends AppCompatActivity {
-
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_create_puzzle);
+
+        Button backButton = (Button) findViewById(R.id.button7);
+
+        backButton.setOnClickListener (new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                goToMainMenuActivity();
+            }
+} );
+        }
+
+    private void goToMainMenuActivity()
+    {
+        Intent intent = new Intent(this, MainMenuActivity.class);
+        startActivity(intent);
     }
 }
 
