0e236b3d7b0a82e277c925856ad513c1a0ce3f04
blueroes112 <ireneyang112@gmail.com>
2017-10-23 10:52:37 -0700

    changed level selection activity and linked the back button

--- app/src/main/java/com/example/claudiaalamillo/nonogamesproject/LevelSelection.java
+++ app/src/main/java/com/example/claudiaalamillo/nonogamesproject/LevelSelection.java
@@ -1,7 +1,10 @@
 package com.example.claudiaalamillo.nonogamesproject;
 
+import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 
 public class LevelSelection extends AppCompatActivity {
 
@@ -9,5 +12,19 @@ public class LevelSelection extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_level_selection);
+        Button backToMainMenuButton = (Button) findViewById(R.id.button7);
+
+        backToMainMenuButton.setOnClickListener (new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                goToMainMenuActivity();
+            }
+        } );
+    }
+
+    private void goToMainMenuActivity()
+    {
+        Intent intent = new Intent(this, MainMenuActivity.class);
+        startActivity(intent);
     }
 }
