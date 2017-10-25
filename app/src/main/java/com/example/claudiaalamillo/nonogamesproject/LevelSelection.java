8b847a4d360cfa18f20d152e2a73cc00dd1809df
blueroes112 <ireneyang112@gmail.com>
2017-10-25 14:19:11 -0700

    created login screen

--- app/src/main/java/com/example/claudiaalamillo/nonogamesproject/LevelSelection.java
+++ app/src/main/java/com/example/claudiaalamillo/nonogamesproject/LevelSelection.java
@@ -1,10 +1,10 @@
 package com.example.claudiaalamillo.nonogamesproject;
 
-import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
+import android.content.Intent;
 
 public class LevelSelection extends AppCompatActivity {
 
