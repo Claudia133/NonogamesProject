d7fe231567dacd3aacb746d332f871734e141c96
blueroes112 <ireneyang112@gmail.com>
2017-11-03 17:58:06 -0700

    created a puzzle board as a test; might have a static view of a puzzle for simplicity purposes and time constraints

---
+++ app/src/main/java/com/example/claudiaalamillo/nonogamesproject/PuzzleBoard1.java
@@ -0,0 +1,13 @@
+package com.example.claudiaalamillo.nonogamesproject;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class PuzzleBoard1 extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_puzzle_board);
+    }
+}
