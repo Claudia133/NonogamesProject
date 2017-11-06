203f41d5fdb6027bf6ffa2c8d644ba4be95b8f79
blueroes112 <ireneyang112@gmail.com>
2017-11-06 14:47:13 -0800

    set solution, column hints, row hints

--- app/src/main/java/com/example/claudiaalamillo/nonogamesproject/Easy1Solution.java
+++ app/src/main/java/com/example/claudiaalamillo/nonogamesproject/Easy1Solution.java
@@ -8,11 +8,72 @@ import java.lang.Integer;
  */
 
 class Easy1Solution {
-    List<List<Integer>> rowHints = new ArrayList<List<Integer>>(5);
+    List<List<Integer>> rowHints = new ArrayList<List<Integer>>();
     List<List<Integer>> columnHints = new ArrayList<List<Integer>>(5);
     int [][] solution = new int[5][5];
 
-    ListIterator<Integer> rowIt = rowHints.listIterator();
+    private void setRowHints()
+    {
+        for( int i = 0; i < 5; i++){
+            rowHints.add( new ArrayList<Integer>());
+        }
+
+        rowHints.get(0).add(2);
+        rowHints.get(0).add(2);
+
+        rowHints.get(1).add(1);
+        rowHints.get(1).add(1);
+        rowHints.get(1).add(1);
+
+        rowHints.get(2).add(1);
+        rowHints.get(2).add(1);
+
+        rowHints.get(3).add(1);
+        rowHints.get(3).add(1);
+
+        rowHints.get(4).add(1);
+    }
+
+    private void setColumnHints()
+    {
+        for( int i = 0; i < 5; i++){
+            columnHints.add( new ArrayList<Integer>());
+        }
+
+        columnHints.get(0).add(3);
+
+        columnHints.get(1).add(1);
+        columnHints.get(1).add(1);
+
+        columnHints.get(2).add(1);
+        columnHints.get(2).add(1);
+
+        columnHints.get(3).add(1);
+        columnHints.get(3).add(1);
+
+        columnHints.get(4).add(3);
+    }
+
+    private void setSolution()
+    {
+        solution[0][0] = 1;
+        solution[0][1] = 1;
+        solution[0][3] = 1;
+        solution[0][4] = 1;
+
+        solution[1][0] = 1;
+        solution[1][2] = 1;
+        solution[1][4] = 1;
+
+        solution[2][0] = 1;
+        solution[2][4] = 1;
+
+        solution[3][1] = 1;
+        solution[3][3] = 1;
+
+        solution[4][2] = 1;
+    }
+
 
 
 
---
+++ app/src/main/java/com/example/claudiaalamillo/nonogamesproject/Puzzle1.java
@@ -0,0 +1,8 @@
+package PACKAGE_NAME;
+
+/**
+ * Created by Inuya on 11/3/2017.
+ */
+
+public class Puzzle1 {
+}
