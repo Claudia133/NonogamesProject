5f2412d2152fe8ad21af7d1e220bd0214795e837
Claudia133 <Megenta54>
2017-11-08 13:37:48 -0800

    fixed level selection screen and connected easy level activity to easy button

--- app/src/main/java/com/example/claudiaalamillo/nonogamesproject/Easy2Solution.java
+++ app/src/main/java/com/example/claudiaalamillo/nonogamesproject/Easy2Solution.java
@@ -1,13 +1,13 @@
 package com.example.claudiaalamillo.nonogamesproject;
 
-import java.util.*;
-import java.lang.Integer;
+import java.util.ArrayList;
+import java.util.List;
 
 /**
- * Created by claudiaalamillo on 11/6/17.
+ * Created by claudiaalamillo on 11/8/17.
  */
 
-class Easy1Solution {
+class Easy2Solution {
     List<List<Integer>> rowHints = new ArrayList<List<Integer>>();
     List<List<Integer>> columnHints = new ArrayList<List<Integer>>(5);
     int [][] solution = new int[5][5];
@@ -18,20 +18,18 @@ class Easy1Solution {
             rowHints.add( new ArrayList<Integer>());
         }
 
-        rowHints.get(0).add(2);
-        rowHints.get(0).add(2);
+        rowHints.get(0).add(5);
 
         rowHints.get(1).add(1);
         rowHints.get(1).add(1);
         rowHints.get(1).add(1);
 
-        rowHints.get(2).add(1);
-        rowHints.get(2).add(1);
+        rowHints.get(2).add(5);
 
         rowHints.get(3).add(1);
         rowHints.get(3).add(1);
 
-        rowHints.get(4).add(1);
+        rowHints.get(4).add(3);
     }
 
     private void setColumnHints()
@@ -43,13 +41,13 @@ class Easy1Solution {
         columnHints.get(0).add(3);
 
         columnHints.get(1).add(1);
-        columnHints.get(1).add(1);
+        columnHints.get(1).add(3);
 
-        columnHints.get(2).add(1);
+        columnHints.get(2).add(3);
         columnHints.get(2).add(1);
 
         columnHints.get(3).add(1);
-        columnHints.get(3).add(1);
+        columnHints.get(3).add(3);
 
         columnHints.get(4).add(3);
     }
@@ -58,6 +56,7 @@ class Easy1Solution {
     {
         solution[0][0] = 1;
         solution[0][1] = 1;
+        solution[0][2] = 1;
         solution[0][3] = 1;
         solution[0][4] = 1;
 
@@ -66,13 +65,16 @@ class Easy1Solution {
         solution[1][4] = 1;
 
         solution[2][0] = 1;
+        solution[2][1] = 1;
+        solution[2][2] = 1;
+        solution[2][3] = 1;
         solution[2][4] = 1;
 
         solution[3][1] = 1;
         solution[3][3] = 1;
 
+        solution[4][1] = 1;
         solution[4][2] = 1;
+        solution[4][3] = 1;
     }
-
 }
-
